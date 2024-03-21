fe0f48b6ff0638b622d3cb39d0892f30fe4ca58c
IulianDo <98779028+IulianDo@users.noreply.github.com>
2024-03-21 02:15:04 +0000

    lazy me

--- ecs605/src/Demo.java
+++ ecs605/src/Demo.java
@@ -24,6 +24,7 @@ public class Demo extends Component implements ActionListener {
     int lastOp;
     float scale = 1f;
     float intensify = 1.5f;
+    int shift = 0;
     private BufferedImage bi, biFiltered;   // the input image saved as bi;//
     int w, h;
 
@@ -71,6 +72,7 @@ public class Demo extends Component implements ActionListener {
         lastOp = opIndex;
         opIndex = i;
     }
+
     void setScale(float i) {
         if (i > 2f)
         {
@@ -88,7 +90,6 @@ public class Demo extends Component implements ActionListener {
 
     public void paint(Graphics g) { //  Repaint will call this function so the image will change.
         filterImage();
-        resizeImage(bi);
         g.drawImage(biFiltered, 0, 0,Math.round(w*scale),Math.round(h*scale), null);
     }
 
@@ -178,7 +179,7 @@ public class Demo extends Component implements ActionListener {
 
         int[][][] ImageArray = convertToArray(image);          //  Convert the image to array
 
-        // Image Negative Operation:
+        // Image resize Operation:
         for(int y=0; y<height; y++){
             for(int x =0; x<width; x++){
                 ImageArray[x][y][1] = Math.round(ImageArray[x][y][1] * intensify);  //r
@@ -213,34 +214,71 @@ public BufferedImage PixelVShift(BufferedImage image) {
         int height = image.getHeight();
 
         int[][][] ImageArray = convertToArray(image);          //  Convert the image to array
-
-        // Image Negative Operation:
+        int rmin = (ImageArray[0][0][1] + shift); 
+        int rmax = rmin;
+        int gmin = (ImageArray[0][0][2] + shift); 
+        int gmax = gmin;
+        int bmin = (ImageArray[0][0][3] + shift); 
+        int bmax = bmin;
         for(int y=0; y<height; y++){
             for(int x =0; x<width; x++){
-                ImageArray[x][y][1] = ImageArray[x][y][1] << 2;  //r
-                if (ImageArray[x][y][1] <0){
+                if(y == 0 && x == 0)
+                {
+                    continue;
+                }
+                ImageArray[x][y][1] = ImageArray[x][y][1] + shift;  //r
+                if (ImageArray[x][y][1] < 0){
                     ImageArray[x][y][1] = 0;
                 }
                 else if(ImageArray[x][y][1] > 255){
-                    ImageArray[x][y][1]=255;
+                    ImageArray[x][y][1] = 255;
                 }
-                ImageArray[x][y][2] = ImageArray[x][y][2] << 2;  //g
-                if (ImageArray[x][y][2] <0){
+
+                if (ImageArray[x][y][1] < rmin){
+                    rmin = ImageArray[x][y][1];
+                }
+                else if(ImageArray[x][y][1] > rmax){
+                    rmax = ImageArray[x][y][1];
+                }
+                ImageArray[x][y][2] = ImageArray[x][y][2] + shift;  //g
+                
+                if (ImageArray[x][y][2] < 0){
                     ImageArray[x][y][2] = 0;
                 }
                 else if(ImageArray[x][y][2] > 255){
-                    ImageArray[x][y][2]=255;
+                    ImageArray[x][y][2] = 255;
                 }
-                ImageArray[x][y][3] = ImageArray[x][y][3] << 2;  //b
-                if (ImageArray[x][y][3] <0){
+
+                if (ImageArray[x][y][2] < gmin){
+                    gmin = ImageArray[x][y][2];
+                }
+                else if(ImageArray[x][y][2] > gmax){
+                    gmax = ImageArray[x][y][2];
+                }
+                ImageArray[x][y][3] = ImageArray[x][y][3] + shift;  //b
+                
+                if (ImageArray[x][y][3] < 0){
                     ImageArray[x][y][3] = 0;
                 }
                 else if(ImageArray[x][y][3] > 255){
-                    ImageArray[x][y][3]=255;
+                    ImageArray[x][y][3] = 255;
+                }
+
+                if (ImageArray[x][y][3] < bmin){
+                    bmin = ImageArray[x][y][3];
+                }
+                else if(ImageArray[x][y][3] > bmax){
+                    bmax = ImageArray[x][y][3];
                 }
             }
         }
-
+        for(int y=0; y<height; y++){
+            for(int x =0; x<width; x++){
+                ImageArray[x][y][3]=255*((ImageArray[x][y][3]-bmin)/(bmax-bmin));                
+                ImageArray[x][y][2]=255*((ImageArray[x][y][2]-gmin)/(gmax-gmin));
+                ImageArray[x][y][1]=255*((ImageArray[x][y][1]-rmin)/(rmax-rmin));
+            }
+        }
         return convertToBimage(ImageArray);  // Convert the array to BufferedImage
     }
 
@@ -259,11 +297,36 @@ public BufferedImage PixelVShift(BufferedImage image) {
                 return;
             case 1: biFiltered = ImageNegative(bi); /* Image Negative */
                 return;
-            case 2: biFiltered = resizeImage(bi); /* Resize Image */
+            case 2: 
+                JFrame f = new JFrame("Input");
+                JPanel panel = new JPanel();
+                f.addWindowListener(new WindowAdapter() {
+                    public void windowClosing(WindowEvent e) {}
+                });
+                JTextField intInput = new JTextField(5);
+                intInput.setActionCommand("SetInput");
+                intInput.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent e){
+                    JTextField tf = (JTextField) e.getSource();
+                    try{
+                        intensify = Float.parseFloat(tf.getText());
+                        biFiltered = resizeImage(bi); /* Resize Image */
+                        repaint();
+                        f.dispose();
+                    }
+                    catch (Exception ex) {
+                        System.out.println("Something went wrong.");
+                    }
+                }});
+                panel.add(new JLabel("Input:"));
+                panel.add(intInput);
+                f.add("North", panel);
+                f.pack();
+                f.setVisible(true);
                 return;
             case 3: biFiltered = PixelVShift(bi); /* Pixel Value Shift */
                 return;
-            case 4: biFiltered = PixelVShift(bi); /* Pixel Value Shift */
+            case 4: biFiltered = PixelVShift(bi); /* Pixel Value Shift and Resize */
                 biFiltered = resizeImage(biFiltered); /* Resize Image */
                 return;
             //************************************
@@ -290,10 +353,6 @@ public BufferedImage PixelVShift(BufferedImage image) {
             if (cb.getActionCommand().equals("SetFilter")) {
                 setOpIndex(cb.getSelectedIndex());
                 repaint();
-            }
-            else if (cb.getActionCommand().equals("SetFloat")) {
-                intensify = cb.getSelectedIndex();
-                repaint();
             } else if (cb.getActionCommand().equals("Formats")) {
                 String format = (String) cb.getSelectedItem();
                 File saveFile = new File("savedimage." + format);
@@ -304,8 +363,7 @@ public BufferedImage PixelVShift(BufferedImage image) {
                     saveFile = chooser.getSelectedFile();
                     try {
                         ImageIO.write(biFiltered, format, saveFile);
-                    } catch (IOException ex) {
-                    }
+                    } catch (IOException ex) {}
                 }
             }
         }
@@ -317,27 +375,23 @@ public BufferedImage PixelVShift(BufferedImage image) {
         f.addWindowListener(new WindowAdapter() {
             public void windowClosing(WindowEvent e) {System.exit(0);}
         });
-        Demo de = new Demo();
-        Demo da = new Demo();
-        imagePanel.add(da);
-        imagePanel.add(de);
+        Demo processedImg = new Demo();
+        Demo originalImg = new Demo();
+        imagePanel.add(originalImg);
+        imagePanel.add(processedImg);
         f.add("Center", imagePanel);
         Button undoButton = new Button("Undo");
         undoButton.setActionCommand("Undo");
-        undoButton.addActionListener(de);
-        JComboBox choices = new JComboBox(de.getDescriptions());
+        undoButton.addActionListener(processedImg);
+        JComboBox choices = new JComboBox(processedImg.getDescriptions());
         choices.setActionCommand("SetFilter");
-        choices.addActionListener(de);
-        JComboBox floats = new JComboBox(de.getFormats());
-        floats.setActionCommand("SetFloat");
-        floats.addActionListener(de);
-        JComboBox formats = new JComboBox(de.getFormats());
+        choices.addActionListener(processedImg);
+        JComboBox formats = new JComboBox(processedImg.getFormats());
         formats.setActionCommand("Formats");
-        formats.addActionListener(de);
+        formats.addActionListener(processedImg);
         JPanel panel = new JPanel();
         panel.add(undoButton);
         panel.add(choices);
-        panel.add(floats);
         panel.add(new JLabel("Save As"));
         panel.add(formats);
         f.add("North", panel);
